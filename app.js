117a3b6a85d20b890f84c7dcb49a20d73ed5967e
xavierrice <xavier.rice@gmail.com>
2023-10-05 11:31:21 -0400

    working on Params

--- app.js
+++ app.js
@@ -9,10 +9,15 @@ const app = express();
 app.get('/', (req, res) =>{
   res.status(420).send('Hello World')
 })
+
+
+//Index Route
  
-app.get('/jumping/joyous/jellybean', (req, res) => {                 // i know this isn't what you want? not sure what you are asking?
-    res.send("Congratulations on starting a new project called jumping-joyous-jellybean!")
-})
+app.get('/:verb/:adj/:noun' ), (req, res) => {     
+  const { verb, adj, noun } = req.params
+  console.log(req.params)
+   res.send(`Congratulations on starting a new project called ${verb}-${adj}-${noun}!`)
+}
 
 
 module.exports = app;
