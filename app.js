a275062ac5f0ddf838d9aa53dbf5cdbc6a5e9342
Rich Boyce <122512687+rboyce212@users.noreply.github.com>
2023-10-05 15:25:13 -0400

    5 passed

--- app.js
+++ app.js
@@ -17,5 +17,26 @@ app.get("/:verb/:adjective/:noun", (request, response) => {
   );
 });
 
+// 99 Little Bugs In the Code
+app.get("/bugs", (request, response) => {
+  response.send(
+    "<h1><u>99 little bugs in the code</u></h1>99 little bugs in the code,<br />99 little bugs...<br /><a href='./bugs/101' ><br />pull one down, patch it around...</a>"
+  );
+});
+
+app.get("/bugs/:numberOfBugs", (request, response) => {
+  const { numberOfBugs } = request.params;
+  const newNumberOfBugs = Number(numberOfBugs) + 2;
+  if (numberOfBugs > 199) {
+    response.send(
+      "<h3><font color='red'>Too many bugs!! Start over!</font color></h3>"
+    );
+  } else {
+    response.send(
+      `<strong>...${numberOfBugs} little bugs in the code.</strong><br /><br /> ${numberOfBugs} little bugs in the code,<br />${numberOfBugs} little bugs...<br /><a href="./${newNumberOfBugs}">Pull one down, patch it around...</a>`
+    );
+  }
+});
+
 // EXPORT
 module.exports = app;
