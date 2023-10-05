11006806fbc497fe893ef09fe3a3664f8dbd83ef
Jeremy Montes <jeremymontes@pursuit.org>
2023-10-05 12:17:29 -0400

    completed 99 little bugs

--- app.js
+++ app.js
@@ -7,4 +7,34 @@ app.get("/:verb/:adjective/:noun", (req,res)=>{
     res.send("Congratulations on starting a new project called " + verb +"-"+adjective+"-"+noun+"!")
 })
 
+app.get("/bugs", (req, res) => {
+    res.send(`
+    <body>
+    <h1> 99 little bugs in the code </h1>
+    </br>
+    <a href ="http://localhost:8888/bugs/101">pull one down, patch it around </a>
+     </body>`)
+})
+
+app.get("/bugs/:numberOfBugs", (req,res)=>{
+    const {numberOfBugs} = req.params
+    let addedBugs = Number(numberOfBugs) + 2
+
+    if(numberOfBugs > 200){
+        res.send(`<body>
+        <a href="http://localhost:8888/bugs">Start Over</a>
+        </body>`)
+    }
+    else {
+        
+    res.send(`<body>
+    <h1>
+    ${numberOfBugs} little bugs in the code
+    </h1>
+    </br>
+    <a href ="http://localhost:8888/bugs/${addedBugs}">pull one down, patch it around </a>
+    </body>`)
+    }
+})
+
 module.exports = app
