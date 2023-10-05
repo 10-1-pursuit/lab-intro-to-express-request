e921e2ba3972a43c5442e79fe9b7f6f7ff01ad25
Smille007 <smille007@gmai.com>
2023-10-05 12:48:14 -0400

    gitignore added

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require("express");
 const app = express();
 const PORT = 8888;
 
+app.get('/bugs', (req, res) => {
+    res.send('99 little bugs in the code');
+  });
+
+
+
 
 app.get('/:jumping/:joyous/:jellybean', (req, res) => {
     res.send('Congratulations on starting a new project called jumping-joyous-jellybean!')
