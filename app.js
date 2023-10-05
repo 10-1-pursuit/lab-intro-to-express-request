e8b549f3e4415e1ba31c3b1ba67ef6676fc7cb0e
Jesse Chong <jessechong@pursuit.org>
2023-10-05 16:07:34 -0400

    Still need to work on last project

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const app = express();
+const pokemon = require("./models/pokemon.json")
+// console.log(pokemon[0])
+
+app.get('/', (req, res) => {
+    res.send("Welcome 99 Pokemon")
+})
 
 app.get('/:verb/:adjective/:noun', (req, res) => {
     const { verb, adjective, noun } = req.params;
@@ -12,7 +18,7 @@ app.get('/bugs', (req, res) => {
 })
 
 app.get('/bugs/:numberOfBugs', (req, res) => {
-    const numberOfBugs = parseInt(req.params.numberOfBugs);
+    const numberOfBugs = req.params.numberOfBugs;
     if (numberOfBugs <= 0) {
         res.send('No bugs left!');
     } else if (numberOfBugs <= 200) {
@@ -20,6 +26,33 @@ app.get('/bugs/:numberOfBugs', (req, res) => {
     } else {
         res.send('<a href="/bugs">Start over</a>')
     }
+})
+
+app.get('/pokemon/:indexOfArray', (req, res) => {
+    const { indexOfArray } = req.params;
+    const index = parseInt(indexOfArray)
+    if (index < 0 || index >= pokemon.length) {
+    res.send(`Sorry, no pokemon found at ${indexOfArray}`)
+    } else {
+        res.send(pokemon[index])
+    }
+})
+
+app.get('/pokemon/search', (req, res) => {
+    const { name } = req.query;
+
+    console.log('Searching for:', name);
+
+    const foundPokemon = pokemon.filter((poke) =>
+        poke.name.toLowerCase() === name.toLowerCase()
+    );
+
+    console.log('Found Pokémon:', foundPokemon);
+
+    res.json(foundPokemon || []);
+});
+app.get('/pokemon', (req, res) => {
+    res.send(pokemon)
 })
 
 module.exports = app
