a103fc7d84933fffe819ac21482ff9e579242d38
xavierrice <xavier.rice@gmail.com>
2023-10-05 11:37:59 -0400

    first test done

--- app.js
+++ app.js
@@ -6,18 +6,18 @@ const app = express();
 
 //Routes
 
-app.get('/', (req, res) =>{
-  res.status(420).send('Hello World')
-})
+// app.get('/', (req, res) =>{
+//   res.status(420).send('Hello World')
+// })
 
 
 //Index Route
  
-app.get('/:verb/:adj/:noun' ), (req, res) => {     
+app.get('/:verb/:adj/:noun' , (req, res) => {     
   const { verb, adj, noun } = req.params
   console.log(req.params)
    res.send(`Congratulations on starting a new project called ${verb}-${adj}-${noun}!`)
-}
+});
 
 
 module.exports = app;
