6c4df368257e5a13c67c0b3259a80f0e11bdaada
Oscar Quintanilla <oscarquintanilla@pursuit.org>
2023-10-05 12:56:41 -0400

    99 little bugs in the code complete

--- app.js
+++ app.js
@@ -9,6 +9,29 @@ app.get("/:verb/:adjective/:noun", (req,res) => {
     res.send(`Congratulations on starting a new project called ${projectName}!`);
 })
 
+
+let bugCount = 99;
+
+app.get("/bugs", (req,res) => {
+    res.send(`
+    <p>${bugCount} little bugs in the code<p>
+    <a href="/bugs/${bugCount + 2}">Pull one down, Patch it around</a>
+    `)
+})
+
+app.get("/bugs/:numberOfBugs", (req,res) => {
+    const {numberOfBugs} = req.params
+    const bugCount = parseInt(numberOfBugs)
+    if (bugCount > 200){
+        res.send(`<p>Too many bugs! <a href="/bugs">Start Over</a><p>`)
+    } else {
+        res.send(`
+        <p>${bugCount} little bugs in the code</p>
+        <a href="/bugs/${bugCount + 2}">Pull one down, Patch it around</a>
+        `)
+    }
+})
+
 app.listen(port, () => {
     console.log(`Listening to port ${port}`)
 })
