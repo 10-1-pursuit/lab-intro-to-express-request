19af6acc41dc631d2e31a8f5adb2bc03c5bcb01f
xavierrice <xavier.rice@gmail.com>
2023-10-05 12:29:50 -0400

    working on bugs

--- app.js
+++ app.js
@@ -6,18 +6,31 @@ const app = express();
 
 //Routes
 
-// app.get('/', (req, res) =>{
-//   res.status(420).send('Hello World')
-// })
-
+app.get('/bugs', (req, res) =>{
+  res.status(420).send('Hello World')
+})
 
 //Index Route
  
-app.get('/:verb/:adj/:noun' , (req, res) => {     
-  const { verb, adj, noun } = req.params
-  console.log(req.params)
-   res.send(`Congratulations on starting a new project called ${verb}-${adj}-${noun}!`)
-});
+// app.get('/:verb/:adj/:noun' , (req, res) => {     
+//   const { verb, adj, noun } = req.params
+//   console.log(req.params)
+//    res.send(`Congratulations on starting a new project called ${verb}-${adj}-${noun}!`)
+// });
+
+app.get('/bugs/:numOfBugs', (req, res) =>{
+  let numOfBugs = 99;
+
+  if(numOfBugs < 200){
+    numOfBugs + 2
+
+    res.send(`<h1>${numOfBugs} Little bugs in the code"</h1><br></br><a href="/bugs/${numOfBugs + 2}">pull one down, patch it around"</a>`)
+  } else {
+    res.send(`<a href="/bugs">Start Over</a>`)
+
+  }
+} )
+
 
 
 module.exports = app;
