08a429e6490624bf96390db7feee6a2cb222735a
Natyka Callwood <Natykacallwood@pursuit.org>
2023-10-05 22:40:13 -0400

    Forgot to run the tests, all test minus pokemon/search have passed.

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const pokemon = require("./models/pokemon.json")
 console.log(pokemon[0]);
 
 app.get("/", (req, res) => {
-  res.send("Testing 1 2 1 2");
+  res.send("Welcome 99 Pokemon");
 });
 
 /////// New Project Name Generator ///////
@@ -27,16 +27,16 @@ app.get("/bugs", (req,res)=>{
 app.get("/bugs/:numberOfBugs", (req, res)=>{
     const {numberOfBugs} = req.params;
     let sumOfBugs= Number(numberOfBugs) + Number(2);
-    if(req.params.numberOfBugs <= 200){
+    if(req.params.numberOfBugs < 200){
         res.send(`
         <body>
-            <h1>${numberOfBugs}</h1>
-            <a href="${sumOfBugs}">pull one down, patch it around: ${sumOfBugs}</a>
+            <h1>${numberOfBugs} little bugs in the code </h1>
+            <a href="${sumOfBugs}">Pull one down, patch it around: ${sumOfBugs}</a>
         </body>`) 
     } else {
         res.send(`
         <body>
-        <a href="/bugs">Start over - Go back to Home page</a>
+        <a href="/bugs">Too many bugs!! Start over!</a>
         </body>`)
     }
 })
@@ -51,14 +51,14 @@ app.get("/pokemon/:indexOfArray", (req, res)=>{
     if(pokemon[indexOfArray]){
         res.send(pokemon[indexOfArray])
     } else {
-        res.send("Sorry, no pokemon fount at /pokemon"+[indexOfArray])
+        res.send("Sorry, no pokemon found at "+[indexOfArray])
     }
 })
 
-app.get("/pokemon/:search/:find/:name", (req, res)=>{
+app.get("/pokemon/search", (req, res)=>{
     console.log(req.query) 
-    const {pokename} = req.query
-    res.send(req.params.name + pokename);
+    const {name} = req.query
+    res.send(req.params.search + name);
 
 })
 
