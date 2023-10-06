fa6761612ffa5a6a52c501541889f65c9d75fa57
JackieC1993 <jackiecheung@pursuit.org>
2023-10-06 00:09:02 -0400

    finished the 99 little bugs

--- app.js
+++ app.js
@@ -10,4 +10,32 @@ app.get('/:verb/:adjective/:noun', (req,res)=> {
     res.send(`Congratulations on starting a new project called ${projectName}!`);
     
 })
-module.exports = app
+
+app.get("/bugs", (req, res) => {
+    res.send(`
+    <body>
+    <h1> 99 little bugs in the code </h1>
+    <a href="/bugs/101">pull one down, patch it around</a>
+    </body>
+      `)
+});
+
+app.get("/bugs/:numberOfBugs", (req, res)=>{
+    const {numberOfBugs} = req.params;
+    let sumOfBugs= Number(numberOfBugs) + Number(2);
+    if(req.params.numberOfBugs <= 200){
+        res.send(`
+        <body>
+            <h1>${numberOfBugs}</h1>
+            <a href="${sumOfBugs}">pull one down, patch it around: ${sumOfBugs}</a>
+        </body>`) 
+    } else {
+        res.send(`
+        <body>
+        <a href="/bugs">Start over - Go back to Home page</a>
+        </body>`); 
+}
+});
+app.listen(8888, () => {
+    console.log("Server is running on port 8888");
+});
