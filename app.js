ccd53f3c91a0cb369833b1aede1b9aea7375db15
keniaDE <decoteaukenia@gmail.com>
2023-10-10 01:01:35 -0400

    first excercise complete

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+
+const app = express();
+
+
+const {PORT} = process.env
+
+
+app.get("/:verb/:adjective/:noun", (req, res) => {
+    //     console.log(req.query)
+    const verb = req.params.verb;
+    const adjective = req.params.adjective;
+    const noun = req.params.noun;
+
+    //     const {foo} = req.query;
+    res.send(`Congratulations on starting a new project called ${verb}-${adjective}-${noun}!`);
+});
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+// EXPORT
+module.exports = app;
