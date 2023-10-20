bbac60604ffac17461ad8e77be407f7b21995914
Kenneth-Parker <kennethkavon@gmail.com>
2023-10-20 12:43:01 -0400

    completed basic functionality

--- app.js
+++ app.js
@@ -1,16 +1,69 @@
 const express = require("express");
+const pokemon = require("./models/pokemon.json");
 const app = express();
 
-app.get("/", (request, response) => {
-    response.status(200).send("Welcome 99 Pokemon");
+// Helper function
+const generateBugsResponse = (number) => {
+  return `${number} little bugs in the code<br>${number} little bugs<br><a href="/bugs/${number + 2}">Pull one down, patch it around </a><br> ${number + 2} bugs in the code`;
+};
+
+// Routes
+app.get("/", (req, res) => {
+  res.status(200).send("Welcome 99 Pokemon");
 });
 
 app.get("/:verb/:adjective/:noun", (req, res) => {
   const { verb, adjective, noun } = req.params;
   const projectName = `${verb}-${adjective}-${noun}`;
-  const response = `Congratulations on starting a new project called ${projectName}!`;
-
-  res.send(response);
+  res.send(`Congratulations on starting a new project called ${projectName}!`);
 });
+
+app.get("/bugs", (req, res) => {
+  res.send(generateBugsResponse(99));
+});
+
+app.get("/bugs/:number", (req, res) => {
+  const number = Number(req.params.number);
+  if (number >= 200) {
+    res.send(`
+      ${number} little bugs in the code<br>
+      <a href="/bugs">Too many bugs!! Start over!</a>
+    `);
+  } else {
+    res.send(generateBugsResponse(number));
+  }
+});
+
+app.get("/pokemon", (req, res) => {
+  res.send(pokemon)
+})
+
+
+app.get("/pokemon/search", (req, res) => {
+  const { name } = req.query;
+  const searchedName = name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
+
+  const matchingPokemon = pokemon.filter((poke) => poke.name === searchedName);
+
+  if (matchingPokemon.length > 0) {
+    res.send(matchingPokemon);
+  } else {
+    res.send([]);
+  }
+});
+
+
+app.get("/pokemon/:indexOfArray", (req, res) => {
+  const indexOfArray = req.params.indexOfArray;
+  const selectedPokemon = pokemon[indexOfArray];
+
+  if (selectedPokemon) {
+    res.status(200).send(selectedPokemon);
+  } else {
+    res.status(404).send(`Sorry, no pokemon found at ${indexOfArray}`);
+  }
+});
+
+console.log(pokemon[24]);
 
 module.exports = app;
