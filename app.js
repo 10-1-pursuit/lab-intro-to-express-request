03a7c5cb1c495b4f7c210c159e177197cd8dc439
Rose <i.a.rose.bk@gmail.com>
2023-11-08 10:00:14 -0500

    added route to find pokemone @ index

--- app.js
+++ app.js
@@ -4,19 +4,31 @@ const app = express();
 const pokemon = require("./models/pokemon.json");
 // console.log(pokemon[0]);
 
-app.get('/bugs', (req,res)=>{
-res.send("99 little bugs in the code");
+app.get('/bugs', (req, res) => {
+    res.send("99 little bugs in the code");
 });
 
 
-app.get('/pokemon',(req,res)=> {
-    res.send (pokemon);
+
+
+
+app.get('/pokemon', (req, res) => {
+    res.send(pokemon);
 });
+
+app.get('/pokemon/:indexOfArray', (req, res) => {
+
+    res.send(pokemon[req.params.indexOfArray]);
+});
+
+
+
+
 
 
 app.get("/:running/:silly/:name", (req, res) => {
     res.send("Congratulations on starting a new project called jumping-joyous-jellybean!");
-  });
+});
 
 
 
