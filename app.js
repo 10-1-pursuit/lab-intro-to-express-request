e188cd3fbdbaf724e08772407a675342ec63445f
Rose <i.a.rose.bk@gmail.com>
2023-11-08 09:54:51 -0500

    added route for all pokemon

--- app.js
+++ app.js
@@ -2,11 +2,17 @@ const express = require('express');
 const app = express();
 
 const pokemon = require("./models/pokemon.json");
-console.log(pokemon[0]);
+// console.log(pokemon[0]);
 
 app.get('/bugs', (req,res)=>{
 res.send("99 little bugs in the code");
 });
+
+
+app.get('/pokemon',(req,res)=> {
+    res.send (pokemon);
+});
+
 
 app.get("/:running/:silly/:name", (req, res) => {
     res.send("Congratulations on starting a new project called jumping-joyous-jellybean!");
