3c397e5a86e314e75fe607661556bc4b2472d770
keniaDE <decoteaukenia@gmail.com>
2023-11-16 00:02:45 -0500

    functionatlity for 99 bugs complete

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 
-const {PORT} = process.env
+const { PORT } = process.env
 
 
 app.get("/:verb/:adjective/:noun", (req, res) => {
@@ -16,6 +16,29 @@ app.get("/:verb/:adjective/:noun", (req, res) => {
     res.send(`Congratulations on starting a new project called ${verb}-${adjective}-${noun}!`);
 });
 
+app.get("/bugs", (req, res) => {
+    res.send(`
+    <h1>99 little bugs in the code</h1>
+    <a href="/bugs/101">pull one down, patch it around</a> `)
+});
+
+// app.get("bugs/:101", (req,res ) => {
+//     res.send(`<h1>${numberOfBugs} little bugs in the code</h1>`)
+// });
+app.get('/bugs/:numberOfBugs', (req, res) => {
+    const numberOfBugs = req.params.numberOfBugs
+    if (numberOfBugs > 200) {
+        res.send(` <h1>${numberOfBugs} little bugs in the code</h1>
+    <a href="/bugs">Start over</a>
+    `);
+    }else{
+        const nextNumberOfBugs = numberOfBugs + 2;
+    res.send(`
+      <h1>${numberOfBugs} little bugs in the code</h1>
+      <a href="/bugs/${nextNumberOfBugs}">Pull one down, patch it around</a>`
+      );
+    }
+    });
 
 
 
